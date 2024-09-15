391208c93aeab6658d2ac9edc8524d4d5f4a2eba
Nandhu sunil <nandhusunil377@gmail.com>
2024-09-15 13:27:33 +0530

    contact form completed

--- contact.php
+++ contact.php
@@ -8,46 +8,49 @@
     </div>
 </section>
 
+<section class="bgDiv sectionPadding">
+    <div class="container form-container shadow-lg">
 
-<div class="container mt-5">
-    <form action="https://api.web3forms.com/submit" method="POST">
-        <div class="row mb-3">
-            <!-- Replace with your Access Key -->
-            <input class="form-control" type="hidden" name="access_key" value="a8cdc6bb-cfe6-4438-8066-cda766fd4939">
-            <!-- Your Name -->
-            <div class="col-md-6">
-                <label for="name" class="form-label">First Name</label>
-                <input type="text" class="form-control" id="name" placeholder="" required>
-            </div>
-            <div class="col-md-6">
-                <label for="name" class="form-label">Last Name</label>
-                <input type="text" class="form-control" id="name" placeholder="" required>
-            </div>
-            <!-- Email -->
-            <div class="col-md-6">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="" required>
-            </div>
-            <!-- Phone Number -->
-            <div class="col-md-6">
-                <label for="phone" class="form-label">Phone Number</label>
-                <input type="number" class="form-control" id="phone" placeholder="" required>
-            </div>
-            <!-- Country -->
-            <div class="col-12">
-                <label for="message" class="form-label">Message</label>
-                <textarea id="message" class="form-control" name="message"></textarea>
-            </div>
-            <!-- Honeypot Spam Protection -->
-            <input type="checkbox" name="botcheck" class="hidden" style="display: none;">
+        <form action="https://api.web3forms.com/submit" method="POST">
+            <div class="row mb-3">
+                <!-- Replace with your Access Key -->
+                <input class="form-control" type="hidden" name="access_key" value="a8cdc6bb-cfe6-4438-8066-cda766fd4939">
+                <!-- Your Name -->
+                <div class="col-12 col-md-6">
+                    <label for="FirstName" class="form-label">First Name</label>
+                    <input type="text" class="form-control" id="FirstName" placeholder="" required>
+                </div>
+                <div class="col-12 col-md-6">
+                    <label for="LastName" class="form-label">Last Name</label>
+                    <input type="text" class="form-control" id="LastName" placeholder="" required>
+                </div>
+                <!-- Email -->
+                <div class="col-12 col-md-6">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="" required>
+                </div>
+                <!-- Phone Number -->
+                <div class="col-12 col-md-6">
+                    <label for="phone" class="form-label">Phone Number</label>
+                    <input type="number" class="form-control" id="phone" placeholder="" required>
+                </div>
+                <!-- Country -->
+                <div class="col-12">
+                    <label for="message" class="form-label">Message</label>
+                    <textarea id="message" rows="6" class="form-control" name="message"></textarea>
+                </div>
+                <!-- Honeypot Spam Protection -->
+                <input type="checkbox" name="botcheck" class="hidden" style="display: none;">
 
-        </div>
-
+            </div>
 
 
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
+</section>
 
 
 
