3a7dd04a3760b181801e69860d76883b86e48aa4
Nandhu sunil <nandhusunil377@gmail.com>
2024-09-17 23:44:18 +0530

    added basic structure of our fleet section

---
+++ our-fleet.php
@@ -0,0 +1,249 @@
+<?php include('header.php'); ?>
+<section class="innerPageBannerSec position-relative d-flex justify-content-center align-items-center">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row justify-content-center align-items-center banner-row">
+            <h1>Our Fleets</h1>
+        </div>
+    </div>
+</section>
+
+
+<section class="package-area pt-80 pb-70">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-package mb-30 w-img">
+                    <img src="assets/img/package/package-1.jpg" alt="package-1" />
+                    <div class="package-person">
+                        <img
+                            src="assets/img/package/package-person.jpg"
+                            alt="package-person" />
+                    </div>
+                    <div class="package-content">
+                        <div class="package-info">
+                            <h3><a href="#">15 Seater Hiroof</a></h3>
+                            <p>4 Days, 5 Nights Start From</p>
+                        </div>
+                        <div class="package-bottom d-flex justify-content-between">
+                            <span>$ 3500</span>
+                            <div class="rating">
+                                <ul>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-package mb-30 w-img">
+                    <img src="assets/img/package/package-2.jpg" alt="package-1" />
+                    <div class="package-person">
+                        <img
+                            src="assets/img/package/package-person-2.jpg"
+                            alt="package-person" />
+                    </div>
+                    <div class="package-content">
+                        <div class="package-info">
+                            <h3><a href="#">30 Seater Coaster</a></h3>
+                            <p>4 Days, 5 Nights Start From</p>
+                        </div>
+                        <div class="package-bottom d-flex justify-content-between">
+                            <span>$ 3500</span>
+                            <div class="rating">
+                                <ul>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-package mb-30 w-img">
+                    <img src="assets/img/package/package-3.jpg" alt="package-1" />
+                    <div class="package-person">
+                        <img
+                            src="assets/img/package/package-person-3.jpg"
+                            alt="package-person" />
+                    </div>
+                    <div class="package-content">
+                        <div class="package-info">
+                            <h3><a href="#">33 Rosa Coaster</a></h3>
+                            <p>4 Days, 5 Nights Start From</p>
+                        </div>
+                        <div class="package-bottom d-flex justify-content-between">
+                            <span>$ 3500</span>
+                            <div class="rating">
+                                <ul>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-package mb-30 w-img">
+                    <img src="assets/img/package/package-4.jpg" alt="package-1" />
+                    <div class="package-person">
+                        <img
+                            src="assets/img/package/package-person-4.jpg"
+                            alt="package-person" />
+                    </div>
+                    <div class="package-content">
+                        <div class="package-info">
+                            <h3><a href="#">Brazil – All Stunning</a></h3>
+                            <p>4 Days, 5 Nights Start From</p>
+                        </div>
+                        <div class="package-bottom d-flex justify-content-between">
+                            <span>$ 3500</span>
+                            <div class="rating">
+                                <ul>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-package mb-30 w-img">
+                    <img src="assets/img/package/package-5.jpg" alt="package-1" />
+                    <div class="package-person">
+                        <img
+                            src="assets/img/package/package-person-5.jpg"
+                            alt="package-person" />
+                    </div>
+                    <div class="package-content">
+                        <div class="package-info">
+                            <h3><a href="#">Brazil – All Stunning</a></h3>
+                            <p>4 Days, 5 Nights Start From</p>
+                        </div>
+                        <div class="package-bottom d-flex justify-content-between">
+                            <span>$ 3500</span>
+                            <div class="rating">
+                                <ul>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-package mb-30 w-img">
+                    <img src="assets/img/package/package-6.jpg" alt="package-1" />
+                    <div class="package-person">
+                        <img
+                            src="assets/img/package/package-person-6.jpg"
+                            alt="package-person" />
+                    </div>
+                    <div class="package-content">
+                        <div class="package-info">
+                            <h3><a href="#">Brazil – All Stunning</a></h3>
+                            <p>4 Days, 5 Nights Start From</p>
+                        </div>
+                        <div class="package-bottom d-flex justify-content-between">
+                            <span>$ 3500</span>
+                            <div class="rating">
+                                <ul>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                    <li>
+                                        <span><i class="fas fa-star"></i></span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include('footer.php'); ?>
