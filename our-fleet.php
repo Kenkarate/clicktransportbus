d3665d1f9ed3a15289e949d520697f8b14778c23
Nandhu sunil <nandhusunil377@gmail.com>
2024-09-21 00:33:30 +0530

    almost completed fleet section

--- our-fleet.php
+++ our-fleet.php
@@ -13,45 +13,8 @@
     <div class="container">
 
         <div class="row">
-            <div class="col-xl-4 col-lg-6 col-md-6">
-                <div class="single-package mb-30 w-img">
-                    <img src="assets/img/package/package-1.jpg" alt="package-1" />
-                    <div class="package-person">
-                        <img
-                            src="assets/img/package/package-person.jpg"
-                            alt="package-person" />
-                    </div>
-                    <div class="package-content">
-                        <div class="package-info">
-                            <h3><a href="#">15 Seater Hiroof</a></h3>
-                            <p>4 Days, 5 Nights Start From</p>
-                        </div>
-                        <div class="package-bottom d-flex justify-content-between">
-                            <span>$ 3500</span>
-                            <div class="rating">
-                                <ul>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-4 col-lg-6 col-md-6">
+
+            <!-- <div class="col-xl-4 col-lg-6 col-md-6">
                 <div class="single-package mb-30 w-img">
                     <img src="assets/img/package/package-2.jpg" alt="package-1" />
                     <div class="package-person">
@@ -88,159 +51,78 @@
                         </div>
                     </div>
                 </div>
+            </div> -->
+
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-package mb-30 w-img">
+                    <img src="assets/nexus-assets/images/fleet/12-flet.jpg" alt="package-1" />
+                    <div class="package-content">
+                        <div class="package-info">
+                            <h3><a href="#">12 Seat</a></h3>
+                        </div>
+
+                    </div>
+                </div>
             </div>
             <div class="col-xl-4 col-lg-6 col-md-6">
                 <div class="single-package mb-30 w-img">
-                    <img src="assets/img/package/package-3.jpg" alt="package-1" />
-                    <div class="package-person">
-                        <img
-                            src="assets/img/package/package-person-3.jpg"
-                            alt="package-person" />
-                    </div>
+                    <img src="assets/nexus-assets/images/fleet/13.jpg" alt="package-1" />
+
                     <div class="package-content">
                         <div class="package-info">
-                            <h3><a href="#">33 Rosa Coaster</a></h3>
-                            <p>4 Days, 5 Nights Start From</p>
-                        </div>
-                        <div class="package-bottom d-flex justify-content-between">
-                            <span>$ 3500</span>
-                            <div class="rating">
-                                <ul>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                </ul>
-                            </div>
+                            <h3><a href="#">13 Seat</a></h3>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-xl-4 col-lg-6 col-md-6">
                 <div class="single-package mb-30 w-img">
-                    <img src="assets/img/package/package-4.jpg" alt="package-1" />
-                    <div class="package-person">
-                        <img
-                            src="assets/img/package/package-person-4.jpg"
-                            alt="package-person" />
-                    </div>
+                    <img src="assets/nexus-assets/images/fleet/29.jpg" alt="package-1" />
                     <div class="package-content">
                         <div class="package-info">
-                            <h3><a href="#">Brazil – All Stunning</a></h3>
-                            <p>4 Days, 5 Nights Start From</p>
+                            <h3><a href="#">29 Seat</a></h3>
                         </div>
-                        <div class="package-bottom d-flex justify-content-between">
-                            <span>$ 3500</span>
-                            <div class="rating">
-                                <ul>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
             <div class="col-xl-4 col-lg-6 col-md-6">
                 <div class="single-package mb-30 w-img">
-                    <img src="assets/img/package/package-5.jpg" alt="package-1" />
-                    <div class="package-person">
-                        <img
-                            src="assets/img/package/package-person-5.jpg"
-                            alt="package-person" />
-                    </div>
+                    <img src="assets/nexus-assets/images/fleet/35.jpg" alt="package-1" />
                     <div class="package-content">
                         <div class="package-info">
-                            <h3><a href="#">Brazil – All Stunning</a></h3>
-                            <p>4 Days, 5 Nights Start From</p>
+                            <h3><a href="#">35 Seat</a></h3>
                         </div>
-                        <div class="package-bottom d-flex justify-content-between">
-                            <span>$ 3500</span>
-                            <div class="rating">
-                                <ul>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
             <div class="col-xl-4 col-lg-6 col-md-6">
                 <div class="single-package mb-30 w-img">
-                    <img src="assets/img/package/package-6.jpg" alt="package-1" />
-                    <div class="package-person">
-                        <img
-                            src="assets/img/package/package-person-6.jpg"
-                            alt="package-person" />
-                    </div>
+                    <img src="assets/nexus-assets/images/fleet/49.jpg" alt="package-1" />
                     <div class="package-content">
                         <div class="package-info">
-                            <h3><a href="#">Brazil – All Stunning</a></h3>
-                            <p>4 Days, 5 Nights Start From</p>
+                            <h3><a href="#">49 Seat</a></h3>
                         </div>
-                        <div class="package-bottom d-flex justify-content-between">
-                            <span>$ 3500</span>
-                            <div class="rating">
-                                <ul>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                    <li>
-                                        <span><i class="fas fa-star"></i></span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <div class="single-package mb-30 w-img">
+                    <img src="assets/nexus-assets/images/fleet/66.jpg" alt="package-1" />
+                    <div class="package-content">
+                        <div class="package-info">
+                            <h3><a href="#">66 Seat</a></h3>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
     </div>
 </section>
