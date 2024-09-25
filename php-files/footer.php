a32dd7c94e89e712cc196eb465f7717335249d1b
kenkarate <kenkarateindia@gmail.com>
2024-09-25 22:24:38 +0530

    content changes

--- php-files/footer.php
+++ php-files/footer.php
@@ -240,14 +240,14 @@
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">City Tours and Picknics</h5>
+        <h5 class="modal-title" id="exampleModalLabel">City Tours & Picnics</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
         <p>
-          Click Passenger Transport UAE provides top–notch office bus rental services for City Tours and Picknicss in UAE. Our service is designed to provide a comfortable and convenient way to explore the city and its attractions.
+          Click Passenger Transport UAE provides top–notch office bus rental services for City Tours & one-day picnics in UAE. Our service is designed to provide a comfortable and convenient way to explore the city and its attractions.
         </p>
       </div>
       <div class="modal-footer">
